455a146757ef5be34642f84cf10c629df9688ba8
Swapna <snanda@cloudbees.com>
2025-03-25 20:59:39 +0530

    index2

--- jelly/jelly/src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
+++ jelly/jelly/src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -1,8 +1,13 @@
 package io.jenkins.plugins.sample;
 
 import hudson.model.Action;
+import hudson.model.RootAction;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerProxy;
+import org.kohsuke.stapler.StaplerRequest;
 
-public class HelloWorldAction implements Action {
+public class HelloWorldAction implements RootAction{
     private String name;
 
     public HelloWorldAction(String name) {
@@ -25,4 +30,5 @@ public class HelloWorldAction implements Action {
     public String getUrlName() {
         return "greeting";
     }
+
 }
